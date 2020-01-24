ad802cdecb00f024c325bc84e56e4f6917167e49
Meghan Flynn <mflynn333@gmail.com>
2020-01-24 17:52:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,17 +14,18 @@ class Transfer
   end 
   
   def execute_transaction 
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount 
-      receiver.balance += amount 
+    if valid? && status == "pending"
+      if amount < self.sender.balance
+      self.sender.balance -= amount 
+      self.receiver.balance += amount 
       self.status = "complete"
     else 
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
 
     # if status is pending then subtract cash from senders balance, add cash to receivers balance, transaction complete... it can reject if sender doesnt have enough funds.
-  end 
-end 
+      end 
+    end 
 
     def reverse_transfer 
     end 
